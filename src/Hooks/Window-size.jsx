e075dd2bd6c52d3e46db231361e41cee4f52c9c6
GamalBasuoney <gamalbasuoneygb.gb@gmail.com>
2025-04-28 12:29:20 +0300

    Update project with latest changes

--- src/Hooks/Window-size.jsx
+++ src/Hooks/Window-size.jsx
@@ -1,25 +1,31 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
+
 export default function useWindowSize() {
   const [size, setSize] = useState({
-    width: undefined,
-    height: undefined,
-    scrollY: undefined,
+    width: 0,
+    height: 0,
+    scrollY: 0,
   });
 
   useEffect(() => {
     function updateSize() {
       setSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-        scrollY: window.pageYOffset,
+        width: typeof window !== "undefined" ? window.innerWidth : 0,
+        height: typeof window !== "undefined" ? window.innerHeight : 0,
+        scrollY: typeof window !== "undefined" ? window.pageYOffset : 0,
       });
     }
 
+    updateSize();
+
+    window.addEventListener("resize", updateSize);
+    window.addEventListener("scroll", updateSize);
+
     return () => {
-      window.addEventListener("resize", updateSize);
-      window.addEventListener("scroll", updateSize);
-      updateSize();
+      window.removeEventListener("resize", updateSize);
+      window.removeEventListener("scroll", updateSize);
     };
   }, []);
+
   return size;
 }
